a1341e6e94dfbbf57bfaea9f542b75f99f96dfa5
Ivan Kuznetsov <Dizzy1986@yandex.ru>
2019-03-26 21:08:18 +0300

    Reduced error level

--- index.php
+++ index.php
@@ -1,18 +1,10 @@
 <?php
 
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 require_once 'vendor/autoload.php';
 
 
 $task = createTask('CompareStringsBySumOfChars');
-
 $task->test();
-
-
-
-
-
-
-
-
-
-
