d125fd8fc479f0e947363be003a5d2c0656e37bc
Ivan Kuznetsov <Dizzy1986@yandex.ru>
2019-03-26 21:08:37 +0300

    https://www.codewars.com/kata/576bb3c4b1abc497ec000065/solutions/php

--- src/CompareStringsBySumOfChars/task.php
+++ src/CompareStringsBySumOfChars/task.php
@@ -25,14 +25,48 @@ class task extends BasicTask
 {
     protected $link = 'https://www.codewars.com/kata/576bb3c4b1abc497ec000065/train/php';
 
-    public function run(...$params)
-    {
-        [$s1, $s2] = func_get_args();
-        var_dump($s1, $s2);
-    }
-
     public function test()
     {
         $this->assertSame(true, $this->run("AD", "BC"));
+    }
+
+    public function optimal()
+    {
+        if (!$a || preg_match('/[^a-z]/i', $a)) {
+            $a = "";
+        }
+        if (!$b || preg_match('/[^a-z]/i', $b)) {
+            $b = "";
+        }
+        list($a, $b) = [strtoupper($a), strtoupper($b)];
+        return array_reduce(str_split($a), function ($s, $c) {
+              return $s + ord($c);
+          }, 0) === array_reduce(str_split($b), function ($s, $c) {
+              return $s + ord($c);
+          }, 0);
+    }
+
+    public function run(...$params)
+    {
+        [$s1, $s2] = func_get_args();
+        return $this->charSum($s1) == $this->charSum($s2);
+    }
+
+    protected function charSum($string)
+    {
+        $string = $this->verify($string);
+
+        return array_reduce(str_split(strtoupper($string)), function ($carry, $item) {
+            return $carry += ord($item);
+        });
+    }
+
+    protected function verify($string)
+    {
+        if ($string === null || preg_match("/[^a-z]/i", $string)) {
+            return '';
+        }
+
+        return $string;
     }
 }
